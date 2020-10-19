74a89728f3b5ab02137507413c5bcd7bd99ed086
Stepan Ponomaryov <stponomarevw@gmail.com>
2020-10-19 22:48:51 +0500

    Task 2 solved

--- js/movies.js
+++ js/movies.js
@@ -3,22 +3,38 @@
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
 console.log(personalMovieDB.movies);
 
-for (let i = 0; i < 2; i++) {
-	let movie = prompt('Один из последних просмотренных фильмов?', '');
-	let star = +prompt('На сколько оцените его?', '');
-	personalMovieDB.movies[movie] = star;
+let i = 0;
+while (i < 2) {
+  let movie = prompt('Один из последних просмотренных фильмов?', '');
+  if (movie === undefined || movie === '' || movie.length > 50) {
+    continue;
+  } else {
+    i++;
+  }
+  let star = +prompt('На сколько оцените его?', '');
+  personalMovieDB.movies[movie] = star;
 }
 
 console.log('*** personalMovieDB fields ***');
 for (let key in personalMovieDB.movies) {
-	console.log(`${key}: ${personalMovieDB.movies[key]}`);
+  console.log(`${key}: ${personalMovieDB.movies[key]}`);
+}
+
+if (numberOfFilms >= 0 && numberOfFilms < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+  alert('Вы классический зритель');
+} else if (numberOfFilms > 30) {
+  alert('Вы киноман');
+} else {
+  alert('Произошла ошибка');
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const reader = require('readline-sync');
+
 // alert('Hello');
 
 // alert('Friend');
@@ -8,22 +10,66 @@
 // const answer = prompt('Do u want to hack smth?');
 // console.log(answer);
 
-const answers = [];
+// const answers = [];
 
 // answers[0] = prompt('Whats your name?', '');
 // answers[1] = prompt('How old are you?', '');
 
 // document.write(answers);
 
-console.log(typeof answers);
+// console.log(typeof answers);
 // console.log(typeof null);
 
-const category = 'joys';
+// const category = 'joys';
 
-console.log(`https://someurl.com/${category}/5`);
+// console.log(`https://someurl.com/${category}/5`);
 
-let incr = 10,
-  decr = 10;
+//let incr = 10,
+//  decr = 10;
 
-incr++;
-console.log(incr);
+//incr++;
+//console.log(incr);
+
+// if (4 === 4) {
+// 	console.log('OK!');
+// }
+
+// let num = 50;
+// let num = reader.question('Number: ');
+
+// if (num < 49) {
+//   console.log('Error');
+// } else if (num > 100) {
+//   console.log('Too much');
+// } else {
+//   console.log('OK!');
+// }
+//
+// num === 50 ? console.log('OK!') : console.log('Error');
+
+// switch (num) {
+//   case 49:
+//   case 51:
+//     console.log('Неверно');
+//     break;
+//   case 50:
+//     console.log('Всё верно');
+//     break;
+//   default:
+//     console.log('Вообще не то');
+//     break;
+// }
+//
+// while (num < 55) {
+//   console.log(num);
+//   num++;
+// }
+//
+// do {
+//   console.log(num);
+//   num++;
+// } while (num < 55);
+
+for (let i = 1; i > 0; i++) {
+  console.log(i);
+}
