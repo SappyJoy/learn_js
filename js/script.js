e18501ebc53177015f43efb205e7e664effaf4f0
Stepan Ponomaryov <stponomarevw@gmail.com>
2020-10-19 16:13:50 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// alert('Hello');
+
+// alert('Friend');
+// const result = confirm('Are u here?');
+
+// const answer = prompt('Do u want to hack smth?');
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Whats your name?', '');
+// answers[1] = prompt('How old are you?', '');
+
+// document.write(answers);
+
+// console.log(typeof answers);
+// console.log(typeof null);
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+let incr = 10,
+  decr = 10;
+
+incr++;
+console.log(incr);
