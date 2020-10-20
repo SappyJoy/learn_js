bfeea4a469f84a9be9aa6da0069417e25040144f
Stepan Ponomaryov <stponomarevw@gmail.com>
2020-10-20 20:10:14 +0500

    Прошёл занятия по функциям обратного вызова и объектам

---
+++ js/objects.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red',
+  },
+  makeTest: function () {
+    console.log('Test');
+  },
+};
+
+// console.log(options.name);
+
+// for (let key in options) {
+//   console.log(`${key}: ${options[key]}`);
+// }
+logObj(options);
+
+/*
+ *   Функция для полного вывода объекта в коносоль
+ * */
+function logObj(obj, tab = 0) {
+  let tabs = '';
+  for (let i = 0; i < tab; i++) {
+    tabs += '  ';
+  }
+  console.log(tabs + '{');
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      console.log(tabs + '  ' + `${key}:`);
+      destructure(obj[key], tab + 1);
+    } else {
+      console.log(tabs + '  ' + `${key}: ${obj[key]},`);
+    }
+  }
+  console.log(tabs + '}');
+}
+
+console.log(Object.keys(options));
+
+const { border, bg } = options.colors;
+console.log(bg);
