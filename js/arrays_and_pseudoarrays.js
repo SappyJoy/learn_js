ecfcdec8f638b2915f19059e89b6307ab43e131b
Stepan Ponomaryov <stponomarevw@gmail.com>
2020-10-20 20:58:45 +0500

    Arrays Done

--- js/arrays_and_pseudoarrays.js
+++ js/arrays_and_pseudoarrays.js
@@ -1,7 +1,34 @@
 'use strict';
 
-const arr = [1, 2, 3, 6, 8];
+const arr = [1, 12, 23, 6, 8];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+  return a - b;
+}
 
 arr.pop();
+arr.push(10);
+arr.shift();
+arr.unshift(4);
 
 console.log(arr);
+
+arr.forEach((item, i, arr) => {
+  console.log(`${i}: ${item} in ${arr}`);
+});
+
+const str = 'test, hello, friend, snake, ball';
+let products = str.split(',');
+products = products.map((item) => {
+  item = item.replace(/\s+/g, '');
+  return item;
+});
+
+console.log(products);
+
+products.sort();
+
+const coolStuff = products.join(';\n');
+console.log(coolStuff);
